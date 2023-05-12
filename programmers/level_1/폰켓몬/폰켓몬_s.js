80308f4ea30339bed0b54b429094ff65865d48a3
SongNoin <as5427072@gmail.com>
2023-05-12 22:25:37 +0900

    week1 폰켓몬 풀이

--- programmers/level_1/폰켓몬/폰켓몬_s.js
+++ programmers/level_1/폰켓몬/폰켓몬_s.js
@@ -1,1 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1845
+function solution(nums) {
+  const pocket = [...new Set(nums)];
+  const selectCnt = Math.abs(nums.length / 2);
+  return pocket.length > selectCnt ? selectCnt : pocket.length;
+}
