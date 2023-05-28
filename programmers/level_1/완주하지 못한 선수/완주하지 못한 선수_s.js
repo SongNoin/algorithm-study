738a3d4542e5caa4f4767d49de9331e3cfa0f3b2
SongNoin <as5427072@gmail.com>
2023-05-28 18:45:33 +0900

    예전 풀이 비교 추가

--- programmers/level_1/완주하지 못한 선수/완주하지 못한 선수_s.js
+++ programmers/level_1/완주하지 못한 선수/완주하지 못한 선수_s.js
@@ -1,3 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42576
+// 예전 풀이
+function solution(participant, completion) {
+  let answer = "";
+  participant.sort((a, b) => (a < b ? -1 : 1));
+  completion.sort((a, b) => (a < b ? -1 : 1));
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      answer = participant[i];
+      return answer;
+    }
+  }
+}
+
+// 재풀이
 function solution(participant, completion) {
   participant.sort((a, b) => (a < b ? -1 : 1));
   completion.sort((a, b) => (a < b ? -1 : 1));
--- programmers/level_1/폰켓몬/폰켓몬_s.js
+++ programmers/level_1/폰켓몬/폰켓몬_s.js
@@ -1,4 +1,23 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1845
+// 예전 풀이
+function solution(nums) {
+  // 폰켓몬들을 담는 배열
+  const pocket = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (
+      pocket.includes(nums[i]) === false &&
+      // pocket 배열의 중복값을 제거
+      pocket.length < nums.length / 2
+      // pocket 에 넣을 수 있는 최대 폰켓몬 값까지만 배열에 추가
+    ) {
+      pocket.push(nums[i]);
+    }
+  }
+  return pocket.length;
+}
+
+// 재풀이
 function solution(nums) {
   const pocket = [...new Set(nums)];
   const selectCnt = Math.abs(nums.length / 2);
