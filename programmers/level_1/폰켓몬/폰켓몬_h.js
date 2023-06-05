be96805516b39114def3627a05921f66b5505821
hyojin <gywlsh274@gmail.com>
2023-06-05 19:31:39 +0900

    week 1 - 문제 풀이

--- programmers/level_1/폰켓몬/폰켓몬_h.js
+++ programmers/level_1/폰켓몬/폰켓몬_h.js
@@ -1,1 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+function solution(nums) {
+  const monsTypesNums = new Set(nums);
+  const newMonsNums = nums.length / 2;
+
+  return newMonsNums < monsTypesNums.size ? newMonsNums : monsTypesNums.size;
+}
